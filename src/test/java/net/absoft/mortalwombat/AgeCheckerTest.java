8710e984112372b40acd75d115e35f22b783b06d
VladSherikch <dizelsw@gmIl.com>
2022-04-16 11:53:06 +0300

    AddCongig

--- src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -9,6 +9,12 @@ public class AgeCheckerTest {
   public void testAgedUserCanPlay() {
     AgeChecker ageChecker = new AgeChecker();
     Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can play game");
-    Assert.assertTrue(ageChecker.testThatTooYongUsersCanNotPlay(16), "Aged user can't play game");
+//    Assert.assertTrue(ageChecker.testThatTooYongUsersCanNotPlay(16), "Aged user can't play game");
   }
+  @Test
+  public void testUnderAgedUserCanPlay() {
+    AgeChecker ageChecker = new AgeChecker();
+    Assert.assertFalse(ageChecker.canPlayGame(17), "Under Aged user can play game");
+  }
+
 }
