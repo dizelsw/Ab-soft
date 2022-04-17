b81cc5b2cdade60b427c1e0abf4ba9cc74856edd
VladSherikch <dizelsw@gmIl.com>
2022-04-17 10:38:31 +0300

    ImplementCalculationQuadraticEquationTest

--- src/main/java/CalculationQuadraticEquation.java
+++ src/main/java/CalculationQuadraticEquation.java
@@ -1,25 +1,22 @@
 import java.util.HashMap;
 
 public class CalculationQuadraticEquation {
-    public HashMap<String, Double> сalculationOfTheRootsOfQuadraticEquation(int a, int b, int c)
-    {
+    public HashMap<String, Double> сalculationOfTheRootsOfQuadraticEquation(int a, int b, int c) {
         double x, y;
         HashMap<String, Double> map = new HashMap<>();
         double d = Math.pow(b, 2) - 4 * a * c;
         if (d > 0) //Decrement > 0
         {
-            x = (-b - Math.sqrt(d))/(2 * a);
-            y = (-b + Math.sqrt(d))/(2 * a);
+            x = (-b - Math.sqrt(d)) / (2 * a);
+            y = (-b + Math.sqrt(d)) / (2 * a);
             map.put("x", x);
             map.put("y", y);
-        }
-        else if (d==0){ //Decrement = 0
-            x = (-b)/(2 * a);
-            y = (-b)/(2 * a);
+        } else if (d == 0) { //Decrement = 0
+            x = (-b) / (2 * a);
+            y = (-b) / (2 * a);
             map.put("x", x);
             map.put("y", y);
-        }
-        else//Decrement < 0
+        } else//Decrement < 0
         {
             map.put("x", null);
             map.put("y", null);
@@ -29,39 +26,35 @@ public class CalculationQuadraticEquation {
 
 
     public boolean possibilityOneRootOfTheEquation(HashMap rootsEquations) {
-        HashMap rootsEquationsTest=rootsEquations;
+        HashMap rootsEquationsTest = rootsEquations;
         try {
-        System.out.println(rootsEquationsTest.get("x"));
-        System.out.println(rootsEquationsTest.get("y"));
-        return (double) rootsEquationsTest.get("x") == (double) rootsEquationsTest.get("y");
-        }
-        catch (NullPointerException e) {
+            System.out.println(rootsEquationsTest.get("x"));
+            System.out.println(rootsEquationsTest.get("y"));
+            return (double) rootsEquationsTest.get("x") == (double) rootsEquationsTest.get("y");
+        } catch (NullPointerException e) {
             System.out.print("NullPointerException caught");
             return false;
         }
     }
 
     public boolean possibilityTwoRootOfTheEquation(HashMap rootsEquations) {
-        HashMap rootsEquationsTest=rootsEquations;
+        HashMap rootsEquationsTest = rootsEquations;
         try {
             System.out.println(rootsEquationsTest.get("x"));
             System.out.println(rootsEquationsTest.get("y"));
             return (double) rootsEquationsTest.get("x") != (double) rootsEquationsTest.get("y");
-        }
-        catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             System.out.print("NullPointerException caught");
             return false;
         }
     }
 
-    public boolean  lackOfEquationRootsOfTheEquation(HashMap rootsEquations) {
-        HashMap rootsEquationsTest=rootsEquations;
+    public boolean lackOfEquationRootsOfTheEquation(HashMap rootsEquations) {
+        HashMap rootsEquationsTest = rootsEquations;
         System.out.println(rootsEquationsTest.get("x"));
         System.out.println(rootsEquationsTest.get("y"));
-        return  rootsEquationsTest.get("x") == null;
+        return rootsEquationsTest.get("x") == null;
     }
-
-
 
 
 }
