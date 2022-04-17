39cde2b691f1a99c08f9f03ef48012c956439a0d
VladSherikch <dizelsw@gmIl.com>
2022-04-17 10:37:08 +0300

    ImplementCalculationQuadraticEquationTest

---
+++ src/main/java/CalculationQuadraticEquation.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+
+public class CalculationQuadraticEquation {
+    public HashMap<String, Double> сalculationOfTheRootsOfQuadraticEquation(int a, int b, int c)
+    {
+        double x, y;
+        HashMap<String, Double> map = new HashMap<>();
+        double d = Math.pow(b, 2) - 4 * a * c;
+        if (d > 0) //Decrement > 0
+        {
+            x = (-b - Math.sqrt(d))/(2 * a);
+            y = (-b + Math.sqrt(d))/(2 * a);
+            map.put("x", x);
+            map.put("y", y);
+        }
+        else if (d==0){ //Decrement = 0
+            x = (-b)/(2 * a);
+            y = (-b)/(2 * a);
+            map.put("x", x);
+            map.put("y", y);
+        }
+        else//Decrement < 0
+        {
+            map.put("x", null);
+            map.put("y", null);
+        }
+        return map;
+    }
+
+
+    public boolean possibilityOneRootOfTheEquation(HashMap rootsEquations) {
+        HashMap rootsEquationsTest=rootsEquations;
+        try {
+        System.out.println(rootsEquationsTest.get("x"));
+        System.out.println(rootsEquationsTest.get("y"));
+        return (double) rootsEquationsTest.get("x") == (double) rootsEquationsTest.get("y");
+        }
+        catch (NullPointerException e) {
+            System.out.print("NullPointerException caught");
+            return false;
+        }
+    }
+
+    public boolean possibilityTwoRootOfTheEquation(HashMap rootsEquations) {
+        HashMap rootsEquationsTest=rootsEquations;
+        try {
+            System.out.println(rootsEquationsTest.get("x"));
+            System.out.println(rootsEquationsTest.get("y"));
+            return (double) rootsEquationsTest.get("x") != (double) rootsEquationsTest.get("y");
+        }
+        catch (NullPointerException e) {
+            System.out.print("NullPointerException caught");
+            return false;
+        }
+    }
+
+    public boolean  lackOfEquationRootsOfTheEquation(HashMap rootsEquations) {
+        HashMap rootsEquationsTest=rootsEquations;
+        System.out.println(rootsEquationsTest.get("x"));
+        System.out.println(rootsEquationsTest.get("y"));
+        return  rootsEquationsTest.get("x") == null;
+    }
+
+
+
+
+}
---
+++ src/test/java/CalculationQuadraticEquationTest.java
@@ -0,0 +1,32 @@
+import net.absoft.mortalwombat.AgeChecker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculationQuadraticEquationTest {
+    @Test
+    public void testHaveTwoRoots() {
+        CalculationQuadraticEquation calculationQuadraticEquation = new CalculationQuadraticEquation();
+        Assert.assertTrue(calculationQuadraticEquation.possibilityTwoRootOfTheEquation(calculationQuadraticEquation
+                .сalculationOfTheRootsOfQuadraticEquation(1,-4,-5)), "Quadratic equation has 2 roots");
+    }
+
+    @Test
+    public void testHaveOneRoots() {
+        CalculationQuadraticEquation calculationQuadraticEquation = new CalculationQuadraticEquation();
+        Assert.assertTrue(calculationQuadraticEquation.possibilityOneRootOfTheEquation(calculationQuadraticEquation
+                .сalculationOfTheRootsOfQuadraticEquation(1,-6,9)), "Quadratic equation has 1 roots");
+    }
+    @Test
+    public void testHaveNorRoots() {
+        CalculationQuadraticEquation calculationQuadraticEquation = new CalculationQuadraticEquation();
+        Assert.assertTrue(calculationQuadraticEquation.lackOfEquationRootsOfTheEquation(calculationQuadraticEquation
+                .сalculationOfTheRootsOfQuadraticEquation(3,-4,2)), "Quadratic equation has not roots");
+    }
+
+    @Test
+    public void testHaveNorRootsCheckException() {
+        CalculationQuadraticEquation calculationQuadraticEquation = new CalculationQuadraticEquation();
+        Assert.assertTrue(calculationQuadraticEquation.possibilityTwoRootOfTheEquation(calculationQuadraticEquation
+                .сalculationOfTheRootsOfQuadraticEquation(3,-4,2)), "Quadratic equation has 2 roots");
+    }
+}
